7af1f484897ee6eaa5caacf0b370776a50f71e86
SYUMJOBA <61433707+SYUMJOBA@users.noreply.github.com>
2021-01-28 16:14:32 +0100

    CREATED THE CENTRAL RELIABLE FUNCTION (and the demo too)

--- Draw scheme.py
+++ Draw scheme.py
@@ -1,7 +1,7 @@
 #this is the definitve program that should include all libs
 import TurtleCells
 
-TurtleCells.drawMacrophage()
+TurtleCells.drawCell("Macrophage")
 
 TurtleCells.move(400, 400)
 TurtleCells.finish()
--- TurtleCells.py
+++ TurtleCells.py
@@ -58,17 +58,44 @@ def drawMacrophage(posX=0, posY=0, scale=1):
     #done
 
 #I'll try making a better function here
-def drawCell(type, posX = 0, posY = 0, scale = 0):
+#here the code works pretty in a complex shape ... basically the turtle loops trough the cell's characteristics to design it, sector by sector.
+#then it will make the central cell role and that's it!
+def drawCell(type, posX = 0, posY = 0, scale = 1):
+    #memorizing the local functions of the cell type
     LocalCellFunctions = Functions_colors.cellsFunction[type]
+    
+    #the wideness of each circular sectors
     radial_angle = 360 / (len(LocalCellFunctions)-1)
-
+    Cell_radius = Functions_colors.cellsSizes[type]
     t.penup()
     t.goto(posX, posY)
-
     t.pendown()
-    for x in range(0, len(LocalCellFunctions)-1):
-        t.forward()
+    
 
+    for x in range(1, len(LocalCellFunctions)):
+        #this chunk of code constructs a circular sector
+        t.color(Functions_colors.colours[LocalCellFunctions[x]]) #here it changes color depending on the function progressively, notice that x is used to loop trough everything
+        t.begin_fill()
+        t.forward(Cell_radius*scale)
+        t.left(90)
+        t.circle(Cell_radius*scale, radial_angle)
+        t.left(90)
+        t.forward(Cell_radius*scale)
+        t.end_fill()
+        t.left(180)
+    
+    CentralCircleWidth = 0.65    #in order for the central circle to be printed smaller inside the entire cellular area this value exists so:
+                                #in order to calculate the raius of the circle, we multipy the entire radius by this constant, nontheless, this value is less than zero because is reduces it.
+
+    #now the central circle is drawn
+    t.penup()
+    t.forward(Cell_radius*CentralCircleWidth)
+    t.color(Functions_colors.colours[LocalCellFunctions[0]])
+    t.pendown()
+    t.left(90)
+    t.begin_fill()
+    t.circle(Cell_radius*CentralCircleWidth)
+    t.end_fill()
     return 0
 
 def finish():
