9e770fc4d53c7bfa42ba2c92855297e002e5a984
SYUMJOBA <61433707+SYUMJOBA@users.noreply.github.com>
2021-01-28 15:17:43 +0100

    created a demo

--- Draw scheme.py
+++ Draw scheme.py
@@ -1,2 +1,9 @@
 #this is the definitve program that should include all libs
+import TurtleCells
 
+TurtleCells.drawMacrophage()
+
+TurtleCells.move(400, 400)
+TurtleCells.finish()
+
+input("press ENTER to exit")
--- TurtleCells.py
+++ TurtleCells.py
@@ -1,8 +1,9 @@
 import turtle
+import Functions_colors
 t = turtle.Turtle()
 
 
-def drawMacrophage(type, posX=0, posY=0, scale=1):
+def drawMacrophage(posX=0, posY=0, scale=1):
 
     #initialize the pen and bring it in position
     t.penup()
@@ -11,7 +12,7 @@ def drawMacrophage(type, posX=0, posY=0, scale=1):
     t.pendown()
 
     #draw the first sector
-    t.color("Cause Inflammation") #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
+    t.color(Functions_colors.colours["Cause inflammation"]) #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
     t.begin_fill()
     t.forward(60*scale)
     t.left(90)
@@ -22,7 +23,7 @@ def drawMacrophage(type, posX=0, posY=0, scale=1):
 
     #draw the second sector
     t.setheading(120)
-    t.color("Activate Cells") #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
+    t.color(Functions_colors.colours["Activate other cells"]) #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
     t.begin_fill()
     t.forward(60*scale)
     t.left(90)
@@ -33,7 +34,7 @@ def drawMacrophage(type, posX=0, posY=0, scale=1):
 
     #draw the third sector
     t.setheading(0)
-    t.color("Communicate") #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
+    t.color(Functions_colors.colours["Communicate"]) #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
     t.begin_fill()
     t.setheading(-120)
     t.forward(60)
@@ -48,10 +49,35 @@ def drawMacrophage(type, posX=0, posY=0, scale=1):
     t.goto(posX, posY)
     t.forward(40*scale)
     t.left(90)
-    t.color("Kill enemies") #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
+    t.color(Functions_colors.colours["Kill enemies"]) #the way that the color is called is not yet definitive, the library with the included colours is yet to be made
     t.pendown()
     t.begin_fill()
     t.circle(40*scale)
     t.end_fill()
 
     #done
+
+#I'll try making a better function here
+def drawCell(type, posX = 0, posY = 0, scale = 0):
+    LocalCellFunctions = Functions_colors.cellsFunction[type]
+    radial_angle = 360 / (len(LocalCellFunctions)-1)
+
+    t.penup()
+    t.goto(posX, posY)
+
+    t.pendown()
+    for x in range(0, len(LocalCellFunctions)-1):
+        t.forward()
+
+    return 0
+
+def finish():
+    turtle.done()
+
+def move(posX, posY):
+    t.penup()
+    t.goto(posX, posY)
+    t.pendown()
+
+def setSpeed(speed):
+    t.speed(speed)
