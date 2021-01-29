c05bf54d3f6c59f53fbd050c8849b10e104ff1d3
SYUMJOBA <61433707+SYUMJOBA@users.noreply.github.com>
2021-01-29 19:21:16 +0100

    Made a larger demo

--- Draw scheme.py
+++ Draw scheme.py
@@ -2,7 +2,7 @@
 import random
 import TurtleCells
 TurtleCells.SetBGColor("black")
-
+TurtleCells.setSpeed(0)
 
 TurtleCells.drawCell("Natural killer cell")
 TurtleCells.drawCell("Macrophage", pos= [200, 100])
@@ -10,10 +10,14 @@ TurtleCells.drawCell("Follicular dendritic cell", pos=[200, 200])
 TurtleCells.drawCell("Neutrofile", pos = [-100, 0])
 TurtleCells.drawCell("Mast cell")
 TurtleCells.drawCell("Macrophage", pos= [-70, -100])
-for x in range(0, 30):
-    a = random.choice([n for n in range(-400, 400)])
-    b = random.choice([n for n in range(-300, 300)])
+TurtleCells.drawCell("B cell", pos = [-300, -200])
+TurtleCells.drawCell("Memory B cell", pos= [-300, -20])
+
+for x in range(0, 60):
+    a = random.choice([n for n in range(-500, 500)])
+    b = random.choice([n for n in range(-400, 400)])
     TurtleCells.drawCell("Antibodies", pos= [ a , b ])
+
 
 
 TurtleCells.move(400, 400)
